7ac6946de83c6f98a327ecd3824cb4b2e4900e27
Marc Prud'hommeaux <mwp1@cornell.edu>
2015-09-24 12:43:58 -0500

    Initial Commit

---
+++ SwiftDojo.playground/Sources/Demo.swift
@@ -0,0 +1,28 @@
+////// Marc Prud'hommeaux - email: marc@glimpse.io - github: mprudhom
+//var i = 1 // var allows re-assignment
+//i++
+//
+//assert(i == 2, "i should have been two")
+//
+//switch i {
+//case 0: "Zero"
+//case 1: "One"
+//case 2...9: "Under 10"
+//default: "Many"
+//}
+//
+//var sum = 0
+//for var j = 1; j <= 10; j++ {
+//    sum += j
+//}
+//sum
+//
+//let str = "Hello, playground" // let disallows re-assignment
+//
+//func exclaim(arg: String) -> String {
+//    return arg + "!"
+//}
+//
+//exclaim(str)
+//
+//assert(exclaim(exclaim("Neato")) == "Neato!!")
