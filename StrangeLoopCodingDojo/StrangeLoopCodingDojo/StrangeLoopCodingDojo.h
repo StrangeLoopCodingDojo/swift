db97784bfc1b6331f52ecbef44a060acff45ebdf
Marc Prud'hommeaux <mwp1@cornell.edu>
2015-09-23 20:23:07 -0500

    Initial commit

---
+++ StrangeLoopCodingDojo/StrangeLoopCodingDojo/StrangeLoopCodingDojo.h
@@ -0,0 +1,19 @@
+//
+//  StrangeLoopCodingDojo.h
+//  StrangeLoopCodingDojo
+//
+//  Created by Marc Prud'hommeaux on 9/23/15.
+//  Copyright © 2015 io.glimpse. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StrangeLoopCodingDojo.
+FOUNDATION_EXPORT double StrangeLoopCodingDojoVersionNumber;
+
+//! Project version string for StrangeLoopCodingDojo.
+FOUNDATION_EXPORT const unsigned char StrangeLoopCodingDojoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StrangeLoopCodingDojo/PublicHeader.h>
+
+
---
+++ StrangeLoopCodingDojo/StrangeLoopCodingDojoTests/StrangeLoopCodingDojoTests.swift
@@ -0,0 +1,37 @@
+//
+//  StrangeLoopCodingDojoTests.swift
+//  StrangeLoopCodingDojoTests
+//
+//  Created by Marc Prud'hommeaux on 9/23/15.
+//  Copyright © 2015 io.glimpse. All rights reserved.
+//
+
+import XCTest
+@testable import StrangeLoopCodingDojo
+
+class StrangeLoopCodingDojoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(1, 2)
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
