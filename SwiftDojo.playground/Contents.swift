ecee6308a88cd2450a5b6a16e877cf772584c45a
Marc Prud'hommeaux <mwp1@cornell.edu>
2015-09-24 13:24:03 -0500

    Minimal V support.

--- SwiftDojo.playground/Contents.swift
+++ SwiftDojo.playground/Contents.swift
@@ -9,12 +9,20 @@ func romanNumeralToIs (roman: String) -> String {
 }
 
 func isToRomanNumeral(i:String) -> String {
-    return i
+    if (i.hasPrefix("IIIII")) {
+        return "V" + isToRomanNumeral(String(i.characters.dropFirst(5)))
+    }
+    switch i {
+        case "IIII": return "IV"
+        default: return i
+    }
 }
 
 assert(addRomanNumerals("I", right: "I") == "II")
 assert(addRomanNumerals("I", right: "II") == "III")
 assert(addRomanNumerals("I", right: "III") == "IV")
+assert(addRomanNumerals("II", right: "III") == "V")
+assert(addRomanNumerals("III", right: "III") == "VI")
 
 
 
