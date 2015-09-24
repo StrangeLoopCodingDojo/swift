41126da362aae7bb2da89b6b7ab16da3c990baeb
Marc Prud'hommeaux <mwp1@cornell.edu>
2015-09-24 13:04:12 -0500

    Implemented basic I's

--- SwiftDojo.playground/Contents.swift
+++ SwiftDojo.playground/Contents.swift
@@ -1,1 +1,17 @@
- 
+func addRomanNumerals (left: String, right: String) -> String {
+    return left + right
+}
+
+func romanNumeralToIs (roman: String) -> String {
+    switch roman {
+    default: return roman
+    }
+}
+
+assert(addRomanNumerals("I", right: "I") == "II")
+assert(addRomanNumerals("I", right: "II") == "III")
+
+
+
+
+
