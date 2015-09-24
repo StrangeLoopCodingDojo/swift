87433a53c77b34e82e87ecf1c028d53f2ef1a820
Marc Prud'hommeaux <mwp1@cornell.edu>
2015-09-24 14:24:45 -0500

    X handling and other stuff

--- SwiftDojo.playground/Contents.swift
+++ SwiftDojo.playground/Contents.swift
@@ -3,7 +3,16 @@ func addRomanNumerals (left: String, right: String) -> String {
 }
 
 func romanNumeralToIs (roman: String) -> String {
+//    let chars = Array(roman.characters)
+    var s = ""
+    let chars = Array (roman.characters)
+    for (i,c) in chars.enumerate(){
+        
+    }
+
     switch roman {
+    case "V" : return "IIIII"
+    case "X" : return "IIIIIIIIII"
     default: return roman
     }
 }
@@ -18,13 +27,20 @@ func isToRomanNumeral(i:String) -> String {
     }
 }
 
+
+romanNumeralToIs("IV")
+romanNumeralToIs("VI")
+stripIsPrefix("IV")
+
 assert(addRomanNumerals("I", right: "I") == "II")
 assert(addRomanNumerals("I", right: "II") == "III")
 assert(addRomanNumerals("I", right: "III") == "IV")
 assert(addRomanNumerals("II", right: "III") == "V")
 assert(addRomanNumerals("III", right: "III") == "VI")
-
-
+assert(romanNumeralToIs("V") == "IIIII")
+assert(romanNumeralToIs("X") == "IIIIIIIIII")
+assert(addRomanNumerals("I", right: "V") == "VI")
+assert(romanNumeralToIs("IV") == "IIII")
 
 
 
